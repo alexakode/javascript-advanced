0ae28cfb90ae4150dc577548d8d3065d7b335657
Alexander <alexanderm.kodehode@gmail.com>
2025-08-26 09:19:29 +0200

    notes

---
+++ script.js
@@ -0,0 +1,9 @@
+// Query parameters
+// limit  ask for a certain number
+// offset  skip a certain number
+// https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
+// e.g. pokemon?limit=10&offset=60
+
+// path parameters
+
+// 304 not modified: use a cached copy because the resource has not been modified since last request
